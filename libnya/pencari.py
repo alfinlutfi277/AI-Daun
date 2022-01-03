c1dc43db885390f13364acfa88f0cf6f2d5f1c0a
Alfin Lutfi Sidiq <79827091+alfinlutfi277@users.noreply.github.com>
2022-01-03 12:08:02 +0700

    updet

--- libnya/pencari.py
+++ libnya/pencari.py
@@ -1,30 +1,31 @@
 import numpy as np
 import csv
 
+
 class Searcher:
-	def __init__(self, indexPath):
-		self.indexPath = indexPath
+    def __init__(self, indexPath):
+        self.indexPath = indexPath
 
-	def search(self, queryFeatures, limit = 5):
-		results = {}
+    def search(self, queryFeatures, limit=5):
+        results = {}
 
-		with open(self.indexPath) as f:
-			reader = csv.reader(f)
+        with open(self.indexPath) as f:
+            reader = csv.reader(f)
 
-			for row in reader:
-				features = [float(x) for x in row[1:]]
-				d = self.chi2_distance(features, queryFeatures)
+            for row in reader:
+                features = [float(x) for x in row[1:]]
+                d = self.chi2_distance(features, queryFeatures)
 
-				results[row[0]] = d
+                results[row[0]] = d
 
-			f.close()
+            f.close()
 
-		results = sorted([(v, k) for (k, v) in results.items()])
+        results = sorted([(v, k) for (k, v) in results.items()])
 
-		return results[:limit]
+        return results[:limit]
 
-	def chi2_distance(self, histA, histB, eps = 1e-10):
-		d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
-			for (a, b) in zip(histA, histB)])
+    def chi2_distance(self, histA, histB, eps=1e-10):
+        d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
+                          for (a, b) in zip(histA, histB)])
 
-		return d
+        return d
