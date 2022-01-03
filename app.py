9f0afdbd26c189d5b79075b37e6c098334e91822
Alfin Lutfi Sidiq <79827091+alfinlutfi277@users.noreply.github.com>
2022-01-03 13:05:47 +0700

    u

--- app.py
+++ app.py
@@ -1,111 +1,73 @@
-from libnya.colordescriptor import ColorDescriptor
-from libnya.pencari import Searcher
-from PIL import Image
-import numpy as np
-import cv2
-import os
-import shutil
-import time
-import csv
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, Request, render_template
 from werkzeug.utils import secure_filename
 import mtcd
 from PIL import Image
 
+datahasil = os.listdir('static/result/')
+
+UPLOAD_FOLDER = 'static/uploads/'
+RESULT_FOLDER = 'static/result/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
+@app.after_request
+def add_header(r):
+    
+    #Add headers to both force latest IE rendering engine or Chrome Frame,
+    #and also to cache the rendered page for 10 minutes.
+    
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
-@app.route('/')
-def cekawal():
-    if os.path.exists('static/temp') == True:
-        shutil.rmtree('static/temp')
-        shutil.rmtree('static/tmp')
-        return redirect('/home')
-    else:
-        return redirect('/home')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename("queryImg.jpg")
+            data = os.path.join('static/uploads/', 'queryImg.jpg')
+            file.save(data)
+            mtcd.process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('result_file'))
+    return render_template('index.html')
 
-@app.route('/home')
-def home():
-    with open('conf.csv', 'r') as conf:
-        tp = 0
-        tn = 0
-        fp = 0
-        fn = 0
-        baca = csv.DictReader(conf)
-        for l in baca:
-            if l['tp'] == '1':
-                tp += 1
-            elif l['tn'] == '1':
-                tn += 1
-            elif l['fp'] == '1':
-                fp += 1
-            else:
-                fn += 1
-    conf.close()
-    akurasi = round((((tn+tp)/(tn+tp+fn+fp))*100), 2)
-    presisi = round(((tp/(fp+tp))*100), 2)
-    rekal = round(((tp/(fn+tp))*100), 2)
-    f1 = round((2*(rekal*presisi)/(rekal+presisi)), 2)
+@app.route('/', methods=['GET', 'POST'])
+def result_file():
+    return render_template('index.html')
 
-    datasets = os.listdir('static/coba')
-    if os.path.exists('static/temp') == True:
-        image_names = os.listdir('static/temp')
-        nearest = sorted(os.listdir('static/temp'))[0]
-        target = os.listdir('static/tmp')
-        return render_template("index.html", f1=(f1), akurasi=(akurasi), presisi=(presisi), rekal=(rekal), image_names=sorted(image_names), target=(target), aw=1, count=len(datasets), nearest=(nearest))
-    else:
-        return render_template("index.html", aw=2, count=len(datasets))
-
-
-@app.route('/search', methods=['POST'])
-def search():
-    gambar = request.files['image']
-    file = gambar.read()
-    nama = gambar.filename[0:3]
-    cd = ColorDescriptor((8, 12, 3))
-    npimg = np.frombuffer(file, np.uint8)
-    query = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-
-    features = cd.describe(query)
-
-    searcher = Searcher('index.csv')
-    results = searcher.search(features)
-
-    positif = ['mou', 'sea', 'des']
-    observ = str(results[0][1])[:3]
-    tp = 1 if nama in positif and nama == observ else 0
-    tn = 1 if nama not in positif and nama != observ else 0
-    fp = 1 if nama not in positif and nama == observ else 0
-    fn = 1 if nama in positif and nama != observ else 0
-    conma = [tp, tn, fp, fn]
-    with open('conf.csv', 'a') as conf:
-        tulis = csv.writer(conf)
-        tulis.writerow(conma)
-    conf.close
-
-    os.makedirs('static/temp')
-    os.makedirs('static/tmp')
-
-    i = 1
-    for (score, resultID) in results:
-        i += 1
-        result = cv2.imread("static/coba/" + resultID)
-        saveimg = cv2.imwrite(
-            "static/temp/" + str(score) + str(i) + ".jpeg", result)
-
-    imgstr = time.strftime("%Y%m%d-%H%M%S")
-    cv2.imwrite("static/tmp/" + imgstr + ".jpeg", query)
-    return redirect("/home")
-
-
-@app.route('/<page_name>')
-def other_page(page_name):
-    response = make_response(
-        'The page named %s does not exist.' % page_name, 404)
-    return response
-
+@app.route('/predict', methods=['GET', 'POST'])
+def mtcd_predict():
+    if request.method == 'POST':
+        filename = request.form.get('input_image')
+        if filename and allowed_file(filename):  
+            img = Image.open(filename)
+            # img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+            img = img.save("static/uploads/queryImg.jpg")             
+            data = os.path.join('static/uploads/queryImg.jpg')
+            mtcd.process_file(data)
+            return redirect('/')
+    return render_template('/index.html', len = len(datahasil), datahasil = datahasil)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
